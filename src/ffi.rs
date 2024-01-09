c22b7bb6a6eb11fcd9b528eda0929b744835fcbd
Igor Martayan <igor@martayan.org>
2024-01-09 15:25:01 +0100

    Cleanup

--- src/ffi.rs
+++ src/ffi.rs
@@ -6,6 +6,7 @@ use autocxx::prelude::*;
 include_cpp! {
     #include "rank_bv.cc"
     #include "tiered_vec.cc"
+
     generate!("RankBV")
     generate!("TieredVec16")
     generate!("TieredVec20")
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![allow(unused_imports)]
 
-// mod rank_bv;
-// mod tiered_vec;
 mod ffi;
 
 use autocxx::WithinUniquePtr;
@@ -10,12 +8,10 @@ use ffi::*;
 fn main() {
     println!("Hi ffi!");
 
-    // let rbv = rank_bv::RankBV::new(20).within_unique_ptr();
     let rbv = RankBV::new(20).within_unique_ptr();
     rbv.set(12);
     assert!(rbv.get(12));
 
-    // let tv = tiered_vec::TieredVec28::new().within_unique_ptr();
     let tv = TieredVec28::new().within_unique_ptr();
     tv.insert(0, 42);
     assert_eq!(tv.get(0), 42);
