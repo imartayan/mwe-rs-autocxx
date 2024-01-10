5c101de0f2f2108c6bf56be7e9c9edbd81dde519
Igor Martayan <igor@martayan.org>
2024-01-10 11:20:04 +0100

    Use submodules for C++ libraries

--- build.rs
+++ build.rs
@@ -7,6 +7,7 @@ fn main() -> miette::Result<()> {
         .build()?;
     b.flag_if_supported("-std=c++17")
         .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-#pragma-messages")
         .compile("autocxx-demo"); // arbitrary library name, pick anything
     println!("cargo:rerun-if-changed=src/ffi.rs");
 
